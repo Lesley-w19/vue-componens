712e9044413f099a6f4111c27fb7dcf1071670fa
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-08-31 12:53:10 +0300

    Added FormChild file and  an emmiting function

---
+++ src/components/FormChild.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="field">
+    <div class="control">
+      <input
+        class="input"
+        type="text"
+        placeholder="Enter your profession"
+        @input="info"
+      />
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  methods: {
+    info(event) {
+      //emit event - name, description
+      this.$emit("addInfo", event.target.value);
+    },
+  },
+};
+</script>
